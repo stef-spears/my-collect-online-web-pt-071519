3d631582d704fe94787683e56e221e505fac6b85
Stef Spears <spears.sj87@gmail.com>
2019-07-31 05:23:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,15 @@
+require 'pry'
+
 def my_collect(array)
 collection = []
 i = 0 
   while i < array.length
+  binding.pry
     yield(array[i])
   end
   collection
 end
 
+array = ["Tim Jones", "Tom Smith", "Jim Camp"]
+
+my collect
