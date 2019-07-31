fb90aead6be1aae3cd62a81188a4f0a91de89333
Stef Spears <spears.sj87@gmail.com>
2019-07-31 05:29:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
 
+def my_collect(array)
+collection = []
+i = 0 
+  while i < array.length
+    collection << yield(array[i])
+    i += 1 
+  end
+  collection
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Camp"]
+
+my_collect(array) do |name|
+  name.split(" ").first 
+end
